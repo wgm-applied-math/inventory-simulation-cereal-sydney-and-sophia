baf102be1b72ccc12da15cdde1b9ad5ae50523c7
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2024-02-27 17:09:12 -0500

    Forgot to add Event.m

---
+++ Event.m
@@ -0,0 +1,27 @@
+classdef (Abstract=true) Event < matlab.mixin.Copyable
+    % Event Abstract base class for all events.
+
+    properties
+        % Time - Time at which this event happens
+        Time = 0;
+    end
+
+    methods
+        function obj = Event(KWArgs)
+            % Event Event constructor.
+            % Public properties can be specified as named arguments.
+            arguments
+                KWArgs.?Event;
+            end
+            fnames = fieldnames(KWArgs);
+            for ifield=1:length(fnames)
+                s = fnames{ifield};
+                obj.(s) = KWArgs.(s);
+            end
+        end
+    end
+    methods (Abstract=true)
+        % visit - Call a handle_??? event on a target object, passing self
+        visit(obj, target)
+    end
+end
